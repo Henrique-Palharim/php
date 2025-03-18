bb96d36f1afdfabfa629f706259d1baf4eba87ff
ra2457076 <henrique.dornelas@unesp.br>
2025-03-18 20:12:10 -0300

    correção 2

--- aula_18-03/exerc3.php
+++ aula_18-03/exerc3.php
@@ -1,7 +1,7 @@
 <?php 
 
     $php2 = 1; $php3 = 5;
-    $php4 = ($php2 <= $php3) ? "menor igual" : "maior";
-    echo $php4;
-
+    $php4 = ($php2 <= $php3) ? "menor ou igual a" : "maior do que";
+    
+    echo $php2 . " é " . "$php4 $php3";
 ?>
